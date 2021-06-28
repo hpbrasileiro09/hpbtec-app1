e6f5f87cbd2940ca6425d6ffe42595c049b11474
Hernando Brasileiro <hpbrasileiro@hotmail.com>
2021-06-28 18:02:55 -0300

    Define base route

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const books = require("./books")
 
 app.use(express.json()); // -> req.body
 
+app.get("/", function(req, res) {
+    const resume = {
+        description: 'Hpbtec-app1 - Backend',
+        author: 'Hernando de Paula Brasileiro',
+        created_at: '2021-06-28 17:00'
+    };
+    res.json(resume);
+});
+
 app.use("/books", books);
 
 const PORT = process.env.PORT || 5000
