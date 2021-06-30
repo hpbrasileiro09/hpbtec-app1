f232892de54a2f29ab053fbcd5d2d48e4b621b40
Hernando Brasileiro <hernando.brasileiro@unifil.br>
2021-06-29 22:18:29 -0300

    Adding cors right way

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const books = require("./books")
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 app.use(express.json()); // -> req.body
 
