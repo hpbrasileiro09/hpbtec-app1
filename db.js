8b8191d80dbce88c611e9c57a340b8149b467328
Hernando Brasileiro <hernando.brasileiro@unifil.br>
2021-07-01 22:48:38 -0300

    Increase Security

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const Pool = require("pg").Pool;
 
+let connString = process.env.DATABASE_URL
+
 const pool = new Pool({
-    user: "cjddhmbzgvpwaz",
-    password: "22e0218f375d52fd5292f811793fdb147b798c79f992ec3540b874e16a54c01c",
-    database: "ddd8oh051v39pn", 
-    host: "ec2-54-224-120-186.compute-1.amazonaws.com",
+    connectionString : connString,
     port: 5432,
     ssl: { rejectUnauthorized: false }
 });
